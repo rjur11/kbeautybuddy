5c24dce030e66fa5001fd4215e355a3b2801c049
Rana Jurjus <rjurjus11@mail.bw.edu>
2022-03-01 18:27:01 -0500

    add more quiz logic

--- src/App.js
+++ src/App.js
@@ -5,29 +5,35 @@ import Quiz from "./Components/Quiz/Quiz";
 import quizQuestions from "./Utils/quizQuestions";
 import "./index.css";
 import { Route, Switch, Link } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [quizResult, setQuizResult] = useState(null);
   return (
     <div className="App">
       <Switch>
         <Route exact path="/">
           <NavBar />
-          <Quiz
-            questions={quizQuestions}
-            onComplete={(answers, obj) => console.log(answers, obj)}
-          />
-          <div className="main-buttons">
-            <button onClick={() => console.log("Quiz")}>
-              Take Skincare Quiz
-            </button>
-            <button onClick={() => console.log("Profile")}>
-              See Skin Profile
-            </button>
-            <button onClick={() => console.log("Shelf")}>
-              Check Your Skincare Shelf
-            </button>
-          </div>
-          <Shelf />
+          {quizResult === null ? (
+            <Quiz
+              questions={quizQuestions}
+              onComplete={(answers, obj) => setQuizResult(obj)}
+            />
+          ) : (
+            <>
+              <div className="main-buttons">
+                <button onClick={() => setQuizResult(null)}>
+                  {quizResult === null ? "Take " : "Retake "} Skincare Quiz
+                </button>
+                <button onClick={() => console.log("Profile")}>
+                  See Skin Profile
+                </button>
+                <button onClick={() => console.log("Shelf")}>
+                  Check Your Skincare Shelf
+                </button>
+              </div>
+            </>
+          )}
         </Route>
       </Switch>
     </div>
