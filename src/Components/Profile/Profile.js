3b57e41ca967e5f23dc6e4dfbf1b1a142c55329b
Rana Jurjus <rjurjus11@mail.bw.edu>
2022-03-02 13:56:40 -0500

    Add conditional logic for quiz results

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -4,20 +4,30 @@ import "./Profile.css";
 
 // Need conditional logic to ensure quizResult is not null otherwise page breaks on reload
 const Profile = (quizResult) => {
+  const checkForNull = (prop) => {
+    return quizResult !== null && quizResult.results !== null
+      ? quizResult.results[prop]
+      : "______";
+  };
   return (
     <>
       <div className="details">
         <h2 className="profile-title"> Your Skin Profile</h2>
         <div className="skin-results">
           <p>
-            Skin Type: <span>{`${quizResult.results.skinType}`}</span>
+            Skin Type: <span>{checkForNull("skinType")}</span>
           </p>
           <p>
-            Skin Concern: <span>{`${quizResult.results.skinConcerns}`}</span>
+            Skin Concern: <span>{checkForNull("skinConcerns")}</span>
           </p>
         </div>
         <Link to="/quiz">
-          <button className="quiz-btn"> Retake Quiz</button>
+          <button className="quiz-btn">
+            {" "}
+            {quizResult === null || quizResult.results === null
+              ? "Take Quiz"
+              : "Retake Quiz"}
+          </button>
         </Link>
         <Shelf />
       </div>
