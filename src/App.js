f2d0ad163d902a5ffa4ad0000f8257e370997be6
Rana Jurjus <rjurjus11@mail.bw.edu>
2022-03-01 19:16:56 -0500

    Flesh out Profile Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./Components/NavBar/NavBar";
 import Shelf from "./Components/Shelf/Shelf";
 import Quiz from "./Components/Quiz/Quiz";
 import quizQuestions from "./Utils/quizQuestions";
+import Profile from "./Components/Profile/Profile";
 import "./index.css";
 import { Route, Switch, Link } from "react-router-dom";
 import { useState } from "react";
@@ -11,31 +12,28 @@ function App() {
   const [quizResult, setQuizResult] = useState(null);
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/">
-          <NavBar />
-          {quizResult === null ? (
-            <Quiz
-              questions={quizQuestions}
-              onComplete={(answers, obj) => setQuizResult(obj)}
-            />
-          ) : (
-            <>
-              <div className="main-buttons">
-                <button onClick={() => setQuizResult(null)}>
-                  {quizResult === null ? "Take " : "Retake "} Skincare Quiz
-                </button>
-                <button onClick={() => console.log("Profile")}>
-                  See Skin Profile
-                </button>
-                <button onClick={() => console.log("Shelf")}>
-                  Check Your Skincare Shelf
-                </button>
-              </div>
-            </>
-          )}
-        </Route>
-      </Switch>
+      <NavBar />
+      {quizResult === null ? (
+        <Quiz
+          questions={quizQuestions}
+          onComplete={(answers, obj) => setQuizResult(obj)}
+        />
+      ) : (
+        <>
+          <div className="main-buttons">
+            <button onClick={() => setQuizResult(null)}>
+              {quizResult === null ? "Take " : "Retake "} Skincare Quiz
+            </button>
+            <button onClick={() => console.log("Profile")}>
+              See Skin Profile
+            </button>
+            <button onClick={() => console.log("Shelf")}>
+              Check Your Skincare Shelf
+            </button>
+          </div>
+        </>
+      )}
+      <Profile />
     </div>
   );
 }
